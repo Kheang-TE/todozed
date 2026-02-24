bc78f960c7652d633157e820ca5b26e4210f8498
Kheang TE <dev.kheang.te@gmail.com>
2026-02-24 23:55:11 +0100

    refactor: auth class -> check authentication, login, logout and register

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,64 +1,156 @@
-const isAuthenticated = async () => {
+class Auth {
+    
+    constructor() {
+        this.apiUrl = 'http://localhost:8000/api';
+        this.currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    }
 
-    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    fetchApi = async (endpoint, options = {}) => {
 
-    try {
-
-        const response = await fetch('http://localhost:8000/api/me', {
-            method: 'GET',
+        const config = {
+            method: options.method,
             headers: {
                 'Content-Type': 'application/json'
             },
             credentials: 'include'
-        });
+        };
 
-        if (response.ok) {
+        if (config.method !== 'GET' && options.body) {
+            config.body = JSON.stringify(options.body);
+        }
 
-            if (currentPage === 'index.html' || currentPage === 'register.html') {
-                window.location.href = 'board.html';
+        return await fetch(`${this.apiUrl}${endpoint}`, config);
+
+    }
+    
+    isAuthenticated = async () => {
+
+        try {
+
+            const response = await this.fetchApi('/me', { method: 'GET' });
+
+            if (response.ok) {
+
+                if (this.currentPage === 'index.html' || this.currentPage === 'register.html') {
+                    window.location.href = 'board.html';
+                }
+                const data = await response.json();
+                document.querySelector('.profile .username span').textContent = data.email;
+
+            } else {
+
+                if (this.currentPage === 'board.html') {
+                    window.location.href = 'index.html';
+                }
+
             }
-            const data = await response.json();
-            document.querySelector('.profile .username span').textContent = data.email;
 
-        } else {
+        } catch (error) {
 
-            if (currentPage === 'board.html') {
+            console.log('Error authentication:', error);
+
+        }
+    }
+
+    logout = async () => {
+
+        try{
+
+            const response = await this.fetchApi('/logout', { method: 'POST' });
+
+            if(response.ok){
                 window.location.href = 'index.html';
             }
 
+        } catch (error){
+
+            console.log('Error logout:', error.message);
+
+        }
+    }
+
+    login = async (e) => {
+
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email').toLowerCase().trim();
+        const password = formData.get('password');
+
+        try {
+            
+            const response = await this.fetchApi('/login', { method: 'POST', body: { email, password } });
+            const data = await response.json();
+
+            if (response.ok) {
+                window.location.href = 'board.html';
+            } else {
+                alert(data.error || data.message || 'Login failed');
+            }
+
+        } catch (error) {
+
+            alert('Error logging in: ' + error.message);
+
+        }
+    }
+
+    register = async (e) => {
+        
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email').toLowerCase().trim();;
+        const password = formData.get('password');
+        const confirmPassword = formData.get('confirm-password');
+
+        if(password !== confirmPassword) {
+            return alert('Passwords do not match');
         }
 
-    } catch (error) {
+        try {
 
-        console.log('Error authentication:', error);
+            const response = await this.fetchApi('/register', { method: 'POST', body: { email, password, confirmPassword } });
+            const data = await response.json();
 
+            if (response.ok) {
+                window.location.href = 'index.html';
+            } else {
+                alert(data.error || data.message || 'Registration failed');
+            }
+            
+        } catch (error) {
+
+            console.log('Error registering user:', error.message);
+
+        }
     }
-}
 
-const logout = async () => {
-    try{
+    initEvents = () => {
+        // Logout button
+        document.getElementById('logout-button')?.addEventListener('click', this.logout);
 
-        const response = await fetch('http://localhost:8000/api/logout', {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
+        // Login form
+        document.querySelector('#login form')?.addEventListener('submit', async (e) => {
+            this.login(e);
         });
 
-        if(response.ok){
-            window.location.href = 'index.html';
-        }
-
-    } catch (error){
-
-        console.log('Error logout:', error.message);
-
+        // Register form
+        document.querySelector('#register form')?.addEventListener('submit', async (e) => {
+            this.register(e);
+        });
     }
+
+    init = () => {
+        this.isAuthenticated();
+        this.initEvents();
+    }
+
 }
 
-isAuthenticated();
-
-if(document.getElementById('logout-button')){
-    document.getElementById('logout-button').addEventListener('click', logout);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const auth = new Auth();
+    auth.init();
+});
